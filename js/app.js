0e6b5487b8ae3f4109b328ec64082132065c399b
manutejaen <manuelmartinezmarin3m@gmail.com>
2016-09-21 17:47:39 +0200

    Create Repository Angular

---
+++ js/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*  [] after module can be used to define depedent modules as it is shown below*/
+angular.module('F1FeederApp', [
+  'F1FeederApp.services',
+  'F1FeederApp.controllers',
+  'ngRoute'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+  when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+  when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+  otherwise({redirectTo: '/drivers'});
+}]);
---
+++ js/services.js
@@ -0,0 +1,38 @@
+
+"use strict";
+
+
+/* .factory let us to keep information no matter if we change between our views*/
+/* factories let us share information between controllers and memorize status between views*/
+/* also share information between views and avoiding the global variables */
+/* to pack common functions to each controllers as we need them, example an app where we need calculate taxes in differents points of the app*/
+/* so we dont need global variables or copying the same piece of code in many places.*/
+
+angular.module('F1FeederApp.services', [])
+  .factory('ergastAPIservice', function($http) {
+
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverDetails = function(id) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getDriverRaces = function(id) {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    return ergastAPI;
+  });
